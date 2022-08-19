7e5e5f571614d4b452edc5fc782d042bece4e654
wizard503 <josemdg12@gmail.com>
2022-08-18 21:21:40 -0600

    Applying good practices

---
+++ script.js
@@ -0,0 +1,79 @@
+var texto=document.querySelector('textarea');
+var encriptar=document.querySelector(".encriptar");
+var desencriptar=document.querySelector(".desencriptar");
+var copiar=document.querySelector(".copiar");
+var output=document.querySelector(".output");
+var pantalla=document.querySelector(".pantalla").style.display="none";
+
+texto.value='';
+texto.focus();
+
+function btnEncriptar(){
+	var palabras=encript(texto.value);
+	output.value=palabras;
+
+	var cuadro=document.querySelector(".msj").style.display="none";
+	pantalla=document.querySelector(".pantalla").style.display='';
+	texto.value="";
+}
+function encript(palabras){
+	var frase=palabras.toLowerCase();
+	var encriptada="";
+	let matrizCodigo=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+	for(let i=0;i<matrizCodigo.length;i++){
+		if(frase.includes(matrizCodigo[i][0])){
+			frase=frase.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+		}
+	}
+	return frase;
+	// if(palabras!=""){
+	// 	for(var i=0;i<frase.length;i++){
+	// 		if(frase[i]=="a"){
+	// 			encriptada+="ai";
+	// 		}else if(frase[i]=="e"){
+	// 			encriptada+="enter";
+	// 		}else if(frase[i]=="i"){
+	// 			encriptada+="imes";
+	// 		}else if(frase[i]=="o"){
+	// 			encriptada+="ober";
+	// 		}else if(frase[i]=="u"){
+	// 			encriptada+="ufat";
+	// 		}else{
+	// 			encriptada+=frase[i];
+	// 		}
+	// 	}
+	// 	var cuadro=document.querySelector(".msj").style.display="none";
+	// 	pantalla=document.querySelector(".pantalla").style.display='';
+
+	// 	output.value=encriptada
+	// }else{
+		
+	// }
+}
+function desencript(){
+	var palabras=texto.value;
+	var frase=palabras.toLowerCase();
+	if(palabras!=""){
+		var mapLetters = {
+			"ai": "a",
+			"enter": "e",
+			"imes": "i",
+			"ober": "o",
+			"ufat": "u"
+		}
+		var desencriptada=frase.replace(/ai|enter|imes|ober|ufat/gi, function(matched){
+			return mapLetters[matched];
+		});
+	}else{
+		output.value="";		
+	}
+	output.value=desencriptada;
+	texto.value="";
+}
+
+function copi(){
+	output.select();
+	navigator.clipboard.writeText(output.value);
+	//document.execCommand('copy');
+}
