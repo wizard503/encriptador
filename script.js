2d7ec1eca3da9267b7caed3e9065ba6b606cd0f3
wizard503 <josemdg12@gmail.com>
2023-02-04 22:02:27 -0600

    optimizing the code

--- script.js
+++ script.js
@@ -1,29 +1,75 @@
-var texto=document.querySelector('textarea');
-var encriptar=document.querySelector(".encriptar");
-var desencriptar=document.querySelector(".desencriptar");
-var copiar=document.querySelector(".copiar");
-var output=document.querySelector(".output");
-var pantalla=document.querySelector(".pantalla").style.display="none";
+var texto = document.querySelector("textarea");
+var encriptar = document.querySelector(".encriptar");
+var desencriptar = document.querySelector(".desencriptar");
+var copiar = document.querySelector(".copiar");
+var output = document.querySelector(".output");
+var pantalla = (document.querySelector(".pantalla").style.display = "none");
 
-texto.value='';
+texto.value = "";
 texto.focus();
 
-function btnEncriptar(){
-	var palabras=encript(texto.value);
-	output.value=palabras;
+function btnEncriptar() {
+	var palabras = encript(texto.value);
+	output.value = palabras;
 
-	var cuadro=document.querySelector(".msj").style.display="none";
-	pantalla=document.querySelector(".pantalla").style.display='';
-	texto.value="";
+	if (palabras != "") {
+		var palabras = encript(texto.value);
+		output.value = palabras;
+
+		var cuadro = (document.querySelector(".msj").style.display = "none");
+		pantalla = document.querySelector(".pantalla").style.display = "";
+		texto.value = "";
+	} else {
+		var cuadro = (document.querySelector(".msj").style.display = "");
+		pantalla = document.querySelector(".pantalla").style.display ="none";		
+		Swal.fire({
+			icon: "error",
+			iconColor: "#9e039c",
+			background: "#E3E0DE",
+			title: "Oops...",
+			confirmButtonColor: "#9e039c",
+			text: "El campo de texto está vacio, escriba el texto que desea encriptar",
+		});
+	}
 }
-function encript(palabras){
-	var frase=palabras.toLowerCase();
-	var encriptada="";
-	let matrizCodigo=[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+function btnDesencriptar() {
+	var palabras = desencript(texto.value);
+	output.value = palabras;
 
-	for(let i=0;i<matrizCodigo.length;i++){
-		if(frase.includes(matrizCodigo[i][0])){
-			frase=frase.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+	if (palabras != "") {
+		var palabras = desencript(texto.value);
+		output.value = palabras;
+
+		var cuadro = (document.querySelector(".msj").style.display = "none");
+		pantalla = document.querySelector(".pantalla").style.display = "";
+		texto.value = "";
+	} else {
+		var cuadro = (document.querySelector(".msj").style.display = "");
+		pantalla = document.querySelector(".pantalla").style.display ="none";
+		Swal.fire({
+			icon: "error",
+			iconColor: "#9e039c",
+			background: "#E3E0DE",
+			title: "Oops...",
+			confirmButtonColor: "#9e039c",
+			text: "El campo de texto está vacio, escriba el texto que desea encriptar",
+		});
+	}
+}
+function encript(palabras) {
+	var frase = palabras.toLowerCase();
+	var encriptada = "";
+	let matrizCodigo = [
+		["e", "enter"],
+		["i", "imes"],
+		["a", "ai"],
+		["o", "ober"],
+		["u", "ufat"],
+	];
+
+	for (let i = 0; i < matrizCodigo.length; i++) {
+		if (frase.includes(matrizCodigo[i][0])) {
+			frase = frase.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
 		}
 	}
 	return frase;
@@ -48,32 +94,37 @@ function encript(palabras){
 
 	// 	output.value=encriptada
 	// }else{
-		
+
 	// }
 }
-function desencript(){
-	var palabras=texto.value;
-	var frase=palabras.toLowerCase();
-	if(palabras!=""){
-		var mapLetters = {
-			"ai": "a",
-			"enter": "e",
-			"imes": "i",
-			"ober": "o",
-			"ufat": "u"
-		}
-		var desencriptada=frase.replace(/ai|enter|imes|ober|ufat/gi, function(matched){
+function desencript(palabras) {
+	var frase = palabras.toLowerCase();
+	var mapLetters = {
+		ai: "a",
+		enter: "e",
+		imes: "i",
+		ober: "o",
+		ufat: "u",
+	};
+	var desencriptada = frase.replace(
+		/ai|enter|imes|ober|ufat/gi,
+		function (matched) {
 			return mapLetters[matched];
-		});
-	}else{
-		output.value="";		
-	}
-	output.value=desencriptada;
-	texto.value="";
+		}
+	);
+	return desencriptada;
 }
 
-function copi(){
+function copi() {
 	output.select();
 	navigator.clipboard.writeText(output.value);
+	Swal.fire({
+		icon: "success",
+		iconColor: "#9e039c",
+		background: "#E3E0DE",
+		title: "¡Bien!",
+		confirmButtonColor: "#9e039c",
+		text: "Texto copiado correctamente",
+	});
 	//document.execCommand('copy');
 }
